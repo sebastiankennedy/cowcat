8e983602fbce209f4e6bbfa78bba44abffc67dad
luisedware <luisedware@qq.com>
2016-04-16 22:31:20 +0800

    登录模块

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -1,37 +1,32 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <title>{{ $page_title or "AdminLTE Dashboard" }}</title>
-    <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css"/>
-</head>
-<body class="skin-black">
-    <div class="wrapper">
-
-        @include('backend.layout.header')
-        @include('backend.layout.sidebar')
-
-        <div class="content-wrapper">
-            <section class="content-header">
-                <h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <title>{{ $page_title or "AdminLTE Dashboard" }}</title>
+        <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body class="skin-black">
+        <div class="wrapper">
+            @include('backend.layout.header')
+            @include('backend.layout.sidebar')
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <h1>
                     {{ $page_title or "Page Title" }}
                     <small>{{ $page_description or null }}</small>
-                </h1>
-
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                    <li class="active">Here</li>
-                </ol>
-            </section>
-
-            <section class="content">
-                @yield('content')
-            </section>
-
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                        <li class="active">Here</li>
+                    </ol>
+                </section>
+                <section class="content">
+                    @yield('content')
+                </section>
+            </div>
+            @include('backend.layout.footer')
         </div>
-        @include('backend.layout.footer')
-    </div>
-<script src="/assets/js/app.min.js"></script>
-</body>
+        <script src="/assets/js/app.min.js"></script>
+    </body>
 </html>
