3fe2fc94919fa06737310d3a5a292868f0b09372
luisedware <luisedware@qq.com>
2016-05-31 11:08:49 +0800

    用户管理

--- app/Http/Routes/backend.php
+++ app/Http/Routes/backend.php
@@ -3,3 +3,5 @@
 $router->get('menu/search', ['as' => 'menu.search', 'uses' => 'MenuController@search', 'middleware' => ['search']]);
 $router->resource('menu', 'MenuController');
 
+$router->get('user/search', ['as' => 'user.search', 'uses' => 'UserController@search', 'middleware' => ['search']]);
+$router->resource("user", 'UserController');
