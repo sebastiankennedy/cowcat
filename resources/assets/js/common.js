39d6e620cb9706b2cb4ed92dbc0f0e537e7882da
luisedware <luisedware@qq.com>
2016-06-12 15:30:24 +0800

    权限管理

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,15 +1,42 @@
 var Backend = window.Backend || {};
 
-(function(Backend){
+(function (Backend) {
     Backend.ajax = {
-        request: function(params){
+        request: function (params) {
             var params = params || {},
                 _type = params.type || 'POST',
                 _data = params.data || {},
-                _successFunction = params.successFunction || function(){
+                _successTitle = params.successTitle || "操作成功",
+                _successFunction = params.successFunction || function () {
                         window.location.reload();
                     },
-                _errorFunction = params.errorFunction
+                _errorTitle = params.errorTitle || "操作失败",
+                _errorFunction = params.errorFunction || function () {
+                        swal(_errorTitle, 'error');
+                    };
+
+            $.ajax({
+                url: params.href,
+                type: _type,
+                data: _data
+            }).done(function (response) {
+                if (response.status == 1) {
+                    swal({
+                        title: _successTitle,
+                        type: 'success',
+                        confirmButtonColor: '#8CD4F5',
+                        closeOnConfirm: false
+                    }, function () {
+                        _successFunction();
+                    });
+                } else if (response.status == -1) {
+                    swal(data.message, '', 'error');
+                } else {
+                    _errorFunction();
+                }
+            }).fail(function () {
+                swal("客户端请求错误", '', 'error');
+            });
         }
     };
 })(Backend);
