148cd984148646d430ac6a7c46043b406b4366d0
luisedware <luisedware@qq.com>
2016-08-18 17:46:15 +0800

    完善「用户详情」模块

--- app/Services/UploadService.php
+++ app/Services/UploadService.php
@@ -72,6 +72,7 @@ class UploadService
             $info['original_name'] = $this->file->getClientOriginalName();
             if($this->driver == 'local'){
                 $info['url'] = '/uploads/' . $savePath;
+                $info['location'] = 1;
             }
 
             return ['message' => '上传文件成功', 'status' => 1, 'data' => $info];
