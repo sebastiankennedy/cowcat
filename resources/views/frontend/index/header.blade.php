1d4530a03810b7072eefc37f15ccc4a819e786ae
luisedware <luisedware@qq.com>
2016-08-24 16:15:20 +0800

    完善「前端首页」细节

--- resources/views/frontend/index/header.blade.php
+++ resources/views/frontend/index/header.blade.php
@@ -47,7 +47,7 @@
                 <a class="btn btn-transparent" href="https://github.com/luisedware/CowCat" target="_blank">
                     点赞
                 </a>
-                <a class="btn btn-transparent" href="http://o93kt6djh.bkt.clouddn.com/WechatIMG1.png">打赏</a>
+                <a class="btn btn-transparent" href="http://o93kt6djh.bkt.clouddn.com/WechatIMG1.png" data-lightbox="image-0" data-title="CowCat">打赏</a>
             </div>
         </div>
     </div>
