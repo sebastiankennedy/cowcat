aeba007073406210cddf0efbc94f32440a7ac499
luisedware <luisedware@qq.com>
2016-08-26 09:14:06 +0800

    完善「前端首页」细节

--- resources/views/frontend/index/facts.blade.php
+++ resources/views/frontend/index/facts.blade.php
@@ -5,17 +5,17 @@
         <div class="row wow fadeInUp" data-wow-delay=".3s">
             <div class="col-lg-4">
                 <span class="fa fa-eye"></span>
-                <h2 class="number timer">21</h2>
+                <h2 class="number timer">23</h2>
                 <h4>Watch</h4>
             </div>
             <div class="col-lg-4">
                 <span class="fa fa-star"></span>
-                <h2 class="number timer">175</h2>
+                <h2 class="number timer">180</h2>
                 <h4>Star</h4>
             </div>
             <div class="col-lg-4">
                 <span class="fa fa-code-fork"></span>
-                <h2 class="number timer">48</h2>
+                <h2 class="number timer">50</h2>
                 <h4>Fork</h4>
             </div>
         </div>
