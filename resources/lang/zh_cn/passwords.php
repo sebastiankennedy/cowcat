6ec5095de3f3a75e1782ce7b8ce5de615171bf16
luisedware <luisedware@qq.com>
2016-08-29 22:11:52 +0800

    完善「本地化」功能

---
+++ resources/lang/zh_cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '请输入符合条件至少六位的密码.',
+    'reset'    => '密码已重置!',
+    'sent'     => '已发送密码重置链接,请注意邮箱查收!',
+    'token'    => '密码重置令牌无效.',
+    'user'     => "邮箱不存在.",
+
+];
