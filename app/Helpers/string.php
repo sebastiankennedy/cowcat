22a9fe231b80b904ba99a316fc9d32d7ed8b9520
luisedware <luisedware@qq.com>
2016-09-01 11:24:13 +0800

    新增函数「get_diff_time」

--- app/Helpers/string.php
+++ app/Helpers/string.php
@@ -13,3 +13,21 @@ if( ! function_exists('ends_with')){
     }
 }
 
+if( ! function_exists('get_diff_time')){
+    function get_diff_time($timestamp)
+    {
+        $datetime = new DateTime(date('Y-m-d H:i:s', $timestamp));
+        $datetime_now = new DateTime();
+        $interval = $datetime_now->diff($datetime);
+        list($y, $m, $d, $h, $i, $s) = explode('-', $interval->format('%y-%m-%d-%h-%i-%s'));
+        if((($result = $y) && ($suffix = '年前')) ||
+            (($result = $m) && ($suffix = '月前')) ||
+            (($result = $d) && ($suffix = '天前')) ||
+            (($result = $h) && ($suffix = '小时前')) ||
+            (($result = $i) && ($suffix = '分钟前')) ||
+            (($result = $s) && ($suffix = '刚刚'))
+        ){
+            return $suffix != '刚刚' ? $result . $suffix : $suffix;
+        }
+    }
+}
